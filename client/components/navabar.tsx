382935e9960b462fc5b2aaaeaa3e753acaf34f7f
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-04 18:27:40 -0600

    css framework established

--- client/components/navabar.tsx
+++ client/components/navabar.tsx
@@ -4,9 +4,9 @@ import * as React from 'react';
 const NavBar = () => (
   <nav className="navbar">
     <a className="active" href='/'>LtP</a>
-    <a className="active" href='/'>about</a>
-    <a className="active" href='/'>skills</a>
-    <a className="active" href='/'>projects</a>
+    <a href='/'>about</a>
+    <a href='/'>skills</a>
+    <a href='/'>projects</a>
   </nav>
 )
 
