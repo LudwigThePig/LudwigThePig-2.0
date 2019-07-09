a74df966144893007770a9cf113ffe34123ff7cc
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-09 10:00:30 -0600

    added blue theme

--- client/templates/app.tsx
+++ client/templates/app.tsx
@@ -28,7 +28,7 @@ export default class App extends React.Component <IAppProps, IAppState> {
     const history = createMemoryHistory();
     
     return (
-        <div className="app">
+        <div className="app app-blue">
           <div className="jumbo">
             <Switch>
               <Route exact path="/" component={Home} />
