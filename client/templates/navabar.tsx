2a111fedddd2e227f13b88adcb7e095782731eaf
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-09 11:20:46 -0600

    fixed active link dupe class application issue

--- client/templates/navabar.tsx
+++ client/templates/navabar.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 const NavBar = () => (
   <nav className="navbar">
-    <NavLink activeClassName="active" to='/'>LtP</NavLink>
+    <NavLink activeClassName="active" exact={true} to='/'>LtP</NavLink>
     <NavLink activeClassName="active" to='/about'>about</NavLink>
     <NavLink activeClassName="active" to='/projects'>projects</NavLink>
   </nav>
