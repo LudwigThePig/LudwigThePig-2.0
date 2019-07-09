6e793b46a3e913866195c4c126e2950b99277ed5
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-09 09:43:50 -0600

    changed styles naming conventions for color vars

--- server/server.tsx
+++ server/server.tsx
@@ -13,14 +13,13 @@ import { Response, Request, NextFunction } from 'express';
 
 app.use(express.static(path.resolve(__dirname, 'bin')));
 
+app.get('/static', express.static(path.resolve(__dirname, 'bin')))
 
-// app.get('/', express.static(path.resolve(__dirname, 'bin')))
 app.get('/*', (req: Request, res: Response): void => {
-
   const htmlPath = path.join(__dirname, 'bin', 'index.html');
   fs.readFile('./bin/main.css', 'utf-8', (cssErr:any, css:any)=> {
     fs.readFile('./bin/index.html', 'utf-8', (htmlErr:any, html:any)=> {
-      res.send(renderer(html, css, req.url));
+      res.send( renderer(html, css, req.url) );
     })
   });
 });
