ee0032e8dfd0e0c5022e2341764f0f9e14e5e2ef
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-12 19:04:05 -0600

    completed schema.sql

--- schema.sql
+++ schema.sql
@@ -1,8 +1,20 @@
 DROP DATABASE ltp IF EXISTS;
 CREATE DATABASE ltp;
 
+DROP TABLE projects IF EXISTS;
+CREATE TABLE projects(
+  id SERIAL NOT NULL,
+  name VARCHAR NOT NULL,
+  description VARCHAR,
+  url VARCHAR NOT NULL,
+  image_url VARCHAR,
+  created DATE NOT NULL,
+  updated DATE,
+  cat_id SMALLINT
+);
 
-CREATE TABLE projects;
-
-
-CREATE TABLE categories;
+DROP TABLE categories IF EXISTS;
+CREATE TABLE categories(
+  id SERIAL NOT NULL,
+  name VARCHAR NOT NULL
+);
