e20f66496918d05a57f21fe5e649b1f15e48d59f
Morgan Galvin <morgan@ludwigthepig.com>
2019-07-17 18:00:28 -0600

    started mobile responsive layout

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const About = () => (
-  <div>
+  <div id="about">
     <h1>About</h1>
     <h2><a target="_blank" href="https://books.google.com/books?id=XMLwzHXNywcC&lpg=PP1&dq=ceci%20n'est%20pas%20une%20pipe%20foucault&pg=PP1#v=onepage&q=ceci%20n'est%20pas%20une%20pipe%20foucault&f=false">This is not a website</a></h2>
   </div>
--- client/pages/project.tsx
+++ client/pages/project.tsx
@@ -2,15 +2,13 @@ import * as React from 'react';
 import { project } from '../types';
 
 const Project = ({id, name, description, url, image_url, created }: project) => (
-  <div className="project">
+  <div className="project" onClick={() => window.open(url, '_blank')}>
     <img src={`${image_url}`} alt={name} />
-    <h3>{ name }</h3>
-    <p>{description}</p>
-    <div className="project-details">
-      <a href={ url }>See More</a>
+    <h3>
+      { name }
       <span>{ created }</span>
-    </div>
-
+    </h3>
+    <p>{description}</p>
   </div>
 );
 
