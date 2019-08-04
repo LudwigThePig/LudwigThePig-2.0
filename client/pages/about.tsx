fafa4f2614a65d52898a6891cbfe94bf1f3a8e39
Morgan Galvin <morgan@ludwigthepig.com>
2019-08-04 13:46:38 -0600

    added text to about page

--- client/pages/about.tsx
+++ client/pages/about.tsx
@@ -3,7 +3,15 @@ import * as React from 'react';
 const About = () => (
   <div id="about">
     <h1>About</h1>
-    <h2><a target="_blank" href="https://books.google.com/books?id=XMLwzHXNywcC&lpg=PP1&dq=ceci%20n'est%20pas%20une%20pipe%20foucault&pg=PP1#v=onepage&q=ceci%20n'est%20pas%20une%20pipe%20foucault&f=false">This is not a website</a></h2>
+    <h3><a target="_blank" href="https://books.google.com/books?id=XMLwzHXNywcC&lpg=PP1&dq=ceci%20n'est%20pas%20une%20pipe%20foucault&pg=PP1#v=onepage&q=ceci%20n'est%20pas%20une%20pipe%20foucault&f=false">This is not a website</a>
+    <br />This a playpen, portal, and everything pig related.
+    </h3>
+    <h3>
+      This is not my website. 
+      <br />This is <a href="https://imgur.com/dTjfhQ0.jpg">Ludwig's</a> website. 
+    </h3>
+    <p>If you would like to learn more about Ludwig's creator, checkout the social links below. 
+    <br />If you would like to see how the sausage was made, checkout the <a href="https://github.com/LudwigThePig/LudwigThePig.com-2.0">GitHub repo</a>.</p>
   </div>
 )
 
