f4172fb5af14a50741d05c32fc2ef243f57eafdd
Morgan Galvin <morgan@ludwigthepig.com>
2019-08-06 11:07:05 -0600

    heroku

---
+++ projects.ts
@@ -0,0 +1,151 @@
+
+enum Categories {
+  'client',
+  'typescript',
+  'frontend',
+  'fullstack',
+  'game',
+}
+
+export default [
+  {
+  "name": "LudwigThePig.com",
+  "key": "I Oink A Lot",
+  "description": "This is LudwigThePig.com silly! You can read more about how this site was built with the link below.",
+  "url": "https://github.com/LudwigThePig/LudwigThePig.com-2.0",
+  "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563145503/ludwigthepig-site_yz2kmy.jpg",
+  "created": "2019-07-01",
+  "categories": ["fullstack", "typescript"]
+  }
+  ,  
+  
+  {
+    "name": "Ludwig's Egg Hunt",
+    "key": "I Oink A Lot",
+    "description": "A multiplayer game made with Socket.IO and WebGL. They say Rome wasn't built in a day, but Ludwig's Egg Hunt was",
+    "url": "https://github.com/LudwigThePig/Ludwigs-Egg-Hunt",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563145503/egg_hunt_bdiezd.jpg",
+    "created": "2019-06-22",
+      "categories": ["game", "fullstack"]
+   }
+   , 
+   {
+    "name": "Insurance of the San Juans",
+    "key": "I Oink A Lot",
+    "description": "One of a series of client projects where I migrated a website from a legacy PHP platform to a modern JavaScript platform. The new websites saw an increase in SEO ranking and user retention.",
+    "url": "https://www.insurancesanjuans.com/",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563145503/insurance_of_the_san_juans_khqzf1.jpg",
+    "created": "2018-09-01",
+      "categories": ["client", "frontend"]
+   }
+   , 
+   {
+    "name": "Roaring Fork Insurance",
+    "key": "I Oink A Lot",
+    "description": "One of a series of client projects where I migrated a website from a legacy PHP platform to a modern JavaScript platform. The new websites saw an increase in SEO ranking and user retention.",
+    "url": "https://www.roaringforkins.com/",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563147513/Roaring_Fork_Insurance_isqhdk.jpg",
+    "created": "2018-06-01",
+      "categories": ["client", "frontend"]
+   }
+   , 
+   {
+    "name": "Grey Lady Insurance",
+    "key": "I Oink A Lot",
+    "description": "One of a series of client projects where I migrated a website from a legacy PHP platform to a modern JavaScript platform. The new websites saw an increase in SEO ranking and user retention.",
+    "url": "https://greyladyins.com/",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563145502/grey_lady_insurance_wbsdyh.jpg",
+    "created": "2019-05-01",
+      "categories": ["client", "frontend"]
+   }
+   , 
+   {
+    "name": "Durango Running Club",
+    "key": "I Oink A Lot",
+    "description": "A website for the Durango Running Club where I was a board member and active volunteer.",
+    "url": "https://www.durangorunningclub.org/",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563145503/Durango_Running_Club_nygfxe.jpg",
+    "created": "2018-07-01",
+      "categories": ["client", "frontend"]
+   }
+   , 
+   {
+    "name": "Treology",
+    "key": "I Oink A Lot",
+    "description": "This is one of the projects that I picked up when I was living in New Zealand. I built out the portfolio section of the website and provided some devops maintence.",
+    "url": "https://www.treology.co.nz/treology-portfolio",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563145503/Treology_dilmh5.jpg",
+    "created": "2019-02-01",
+      "categories": ["client", "frontend"]
+   }
+   , 
+   {
+    "name": "Multi Step Checkout",
+    "key": "I Oink A Lot",
+    "description": "A multi step checkout application with form validation and an SQL server",
+    "url": "https://github.com/LudwigThePig/Multistep-Checkout-Experience",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563145502/multistep_checkout_u4iqlp.jpg",
+    "created": "2019-05-01",
+      "categories": ["frontend", "fullstack"]
+   }
+   , 
+   {
+    "name": "Connect 4",
+    "key": "I Oink A Lot",
+    "description": "Connect 4 with a fun 'twist'. Made with React, Node, Express, and SQL",
+    "url": "https://github.com/LudwigThePig/Multistep-Checkout-Experience",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563145501/connect_4_zrmuqv.jpg",
+    "created": "2019-05-01",
+      "categories": ["frontend", "fullstack", "game"]
+   }
+   , 
+   {
+    "name": "Klackers",
+    "key": "I Oink A Lot",
+    "description": "A digital recreation of a classic game that is popular with Durangatangs",
+    "url": "https://github.com/LudwigThePig/klackers",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563145501/Klackers_wzwmk2.jpg",
+    "created": "2018-11-01",
+      "categories": ["frontend", "game"]
+   }
+   , 
+   {
+    "name": "Anonymous Message Board",
+    "key": "I Oink A Lot",
+    "description": "An anonymous message board where users can make posts, leave comments, report posts, and more!",
+    "url": "https://github.com/LudwigThePig/Anonymous-Messageboard",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563145500/Anon_Msg_Board_yncuhm.jpg",
+    "created": "2019-01-01",
+      "categories": ["fullstack"]
+   }
+   , 
+   {
+    "name": "Cloudbnb",
+    "key": "I Oink A Lot",
+    "description": "With a small team, I inherited the front end of this project, built its backend, and deployed it. We scaled the application to handle over 5,000 requests per second on one EC2 instance.",
+    "url": "https://github.com/LudwigThePig/cloudbnb-service",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563145500/cloudbnb_gfvjgv.jpg",
+    "created": "2019-06-01",
+      "categories": ["fullstack"]
+   }
+   , 
+   {
+    "name": "Tarantula Equipment Inc.",
+    "key": "I Oink A Lot",
+    "description": "This is a clone of REI.com that I built out with a small team with a microservices architecture.",
+    "url": "https://github.com/montyjs/tarantula-equpment-inc-product-service",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563149000/Tarantula_Equipment_Inc_swhhu1.jpg",
+    "created": "2019-05-01",
+      "categories": ["frontend", "fullstack"]
+   }
+   , 
+   {
+    "name": "Drum Machine",
+    "key": "I Oink A Lot",
+    "description": "A drum machine made with React!",
+    "url": "https://github.com/LudwigThePig/drum-machine",
+    "image_url": "https://res.cloudinary.com/ludwigthepig/image/upload/v1563145500/Drum_machine_gp16ns.jpg",
+    "created": "2018-12-01",
+      "categories": ["game", "frontend"]
+   }
+]
